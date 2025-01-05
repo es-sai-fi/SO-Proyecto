16ccf2e5b5e5245dec7c608047712ed419ddf094
es-sai-fi <jhormangomez2005@outlook.com>
2025-01-05 18:12:42 -0500

    Final

--- server/server.c
+++ server/server.c
@@ -7,8 +7,44 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <assert.h>
 
-void processInput(char input[], char *args[])
+// La función es exactamente la misma que de_vector_a_cadena en el proyecto minishell del profesor, créditos hacia su persona.
+char **processInput(char *input)
+{
+  int i;
+  char *token; // Variable para almacenar cada token extraído mediante strtok.
+  char *delim = " "; // Delimitador en base al cual se van a separar las tokens.
+  char **resultado;
+
+  // Se asigna inicialmente cierto espacio a resultado.
+  resultado = (char **)malloc(sizeof(char *));
+  assert(resultado != NULL); // Se verifica que fue exitosa la asignación.
+  i = 0;
+  token = strtok(input, delim); // Se extrae el primer token.
+  while (token != NULL) // Este bucle seguira funcionando hasta que no haya tokens por extraer.
+  {
+    int cad_longitud;
+    char **result_temp;
+    cad_longitud = strlen(token) + 1;
+    // Copiar token en la posicion 'i'
+    resultado[i] = strdup(token);
+    // printf("|%s|\n",resultado[i]); fflush(stdout);
+    //  En busca de la proxima cadena
+    token = strtok(NULL, delim); // Se extrae la siguiente token.
+    i++;
+    result_temp = realloc(resultado, (i + 1) * sizeof(*resultado)); // Se aumenta el espacio de result_temp por cada ciclo.
+    assert(result_temp != NULL);
+    resultado = result_temp; // Por lo pronto el resultado final será result_temp.
+  }
+
+  resultado[i] = NULL; // Nos aseguramos que el último elemento sea NULL para que execvp funcione.
+  return resultado;
+
+  // Es un poco complicada la función porque se está trabajando con un tamaño de vector indefinido por lo tanto por cada token escrito se realoca memoria para guardar esa token, es decir, básicamente estamos convirtiendo cadenas de longitud desconocida sin saber su número de tokens (palabras separadas por espacios) en un vector char de tamaño dinámico (mediante realloc).
+}
+
+void processInputCd(char input[], char **args)
 {
   // Si los primeros dos carácteres son cd entonces tratamos al resto del buffer input como el segundo elemento de args.
   if (strncmp(input, "cd", 2) == 0 && (input[2] == ' ' || input[2] == '\0'))
@@ -19,19 +55,6 @@ void processInput(char input[], char *args[])
     args[1] = strtok(input + 3, "");
 
     args[2] = NULL;
-  }
-  else
-  {
-    // Si no es "cd", procesamos el input normalmente.
-    int i = 0;
-    char *arg = strtok(input, " ");
-    while (arg != NULL)
-    {
-      args[i] = arg;
-      i++;
-      arg = strtok(NULL, " ");
-    }
-    args[i] = NULL;
   }
 }
 
@@ -71,52 +94,82 @@ int main(int argc, char const *argv[])
 
   char input[256];
   char output[1024];
-  // Se crea el pipe para redigirir los streams a este.
-  int pipefd[2];
-  pipe2(pipefd, O_NONBLOCK);
-  int keepRunning;
 
-  // Se crea un nuevo proceso para atender el comando a ejecutar.
-  pid_t pid = fork();
+  // Vector donde se guardará la cadena convertida en vector.
+  char **args;
 
-  if (pid == 0) // Hijo original.
+  size_t bytesReceived, bytesRead;
+
+  int keepRunning = 1;
+
+  while (keepRunning)
   {
-    size_t bytesRead, bytesReceived;
-    int keepRunning = 1;
-    // Se crea el vector donde se guardarán los argumentos que usará execvp/chdir.
-    char *args[10];
+    // Nos aseguramos de que los buffers estén "vacíos".
+    memset(input, 0, sizeof(input));
+    memset(output, 0, sizeof(output));
 
-    // Se redirijen los streams STDOUT y STDERR al pipe (streams de hijo original).
-    dup2(pipefd[1], STDOUT_FILENO);
-    dup2(pipefd[1], STDERR_FILENO);
-    // Se cierra el descriptor de escritura del pipe en el proceso hijo original.
-    close(pipefd[1]);
+    // Se crea un pipe para redigir el output y cualquier error ocurrido al padre (se establece como no bloqueante).
+    int pipefd[2];
+    pipe(pipefd);
 
-    while (keepRunning)
+    // Se recibe la cadena.
+    bytesReceived = recv(clientSocket, input, sizeof(input), 0);
+
+    // Se convierte el carácter siguiente al último del input a uno nulo. Esto es importante para funciones como strtok.
+    input[bytesReceived] = '\0';
+
+    if (strcmp(input, "cd") == 0)
     {
-      // Nos aseguramos de que los buffers estén "vacíos".
-      memset(input, 0, sizeof(input));
-      memset(output, 0, sizeof(output));
+      // Se ejecuta otra versión de processInput, esta es para solicitudes de "cd".
+      processInputCd(input, args);
+    }
+    else
+    {
+      // Se ejecuta la función processInput que se encargará de dividir la cadena en elementos de un vector acorde a " ".
+      args = processInput(input);
+    }
 
-      // Nos aseguramos que el vector args sea limpiado en cada iteración.
-      for (int i = 0; i < 10; i++)
+    // Se crea un nuevo proceso para atender el comando a ejecutar.
+    pid_t pid = fork();
+
+    // Proceso hijo.
+    if (pid == 0)
+    {
+      // Se cierra la entrada de lectura del hijo.
+      close(pipefd[0]);
+      // Se redirige el output y cualquier error que genere el hijo hacia el pipe.
+      dup2(pipefd[1], STDOUT_FILENO);
+      dup2(pipefd[1], STDERR_FILENO);
+      // Se cierra la entrada de escritura del hijo.
+      close(pipefd[1]);
+
+      // Si la cadena recibida es "salir" o el primer arg de args es "cd" nos aseguramos que el padre se encargue de estas solicitudes.
+      if (strcmp(args[0], "salir") == 0 || strcmp(args[0], "cd") == 0)
       {
-        args[i] = NULL;
+        exit(0);
       }
-
-      // Se recibe la cadena.
-      bytesReceived = recv(clientSocket, input, sizeof(input), 0);
-
-      // Se convierte el carácter siguiente al último del input a uno nulo. Esto es importante para funciones como strtok.
-      input[bytesReceived] = '\0';
-
-      // Se ejecuta la función processInput que se encargará de dividir la cadena.
-      processInput(input, args);
+      // Se ejecuta el comando mediante execvp.
+      else
+      {
+        // Si execvp falla imprimimos el error.
+        if (execvp(args[0], args) == -1)
+        {
+          perror("Execvp failed");
+          exit(1);
+        }
+      }
+    }
+    // Proceso padre.
+    else if (pid > 0)
+    {
+      // Se espera a que el proceso hijo termine.
+      wait(NULL);
 
       // Si la cadena recibida es "salir" entonces se cambia el valor de "continue" a false.
       if (strcmp(input, "salir") == 0)
       {
-        snprintf(output, sizeof(output), "Saliendo...\n");
+        const char *message = "Saliendo...\n";
+        write(pipefd[1], message, strlen(message));
         keepRunning = 0;
       }
       // Si el comando principal es cd se utiliza chdir.
@@ -124,68 +177,45 @@ int main(int argc, char const *argv[])
       {
         if (args[1] == NULL)
         {
-          snprintf(output, sizeof(output), "No directory specified\n");
+          const char *message = "No directory specified\n";
+          write(pipefd[1], message, strlen(message));
         }
         else
         {
           if (chdir(args[1]) != 0)
           {
-            snprintf(output, sizeof(output), "Chdir failed\n");
+            const char *message = "Chdir failed\n";
+            write(pipefd[1], message, strlen(message));
           }
         }
       }
-      else
-      {
-        pid_t executerChild = fork();
-        if (executerChild == 0) // Nuevo hijo, se encarga de ejecutar comandos con execvp (su padre es el hijo original).
-        {
-          // Se cierran los descriptures del pipe en el proceso hijo ejecutor.
-          close(pipefd[0]);
-          close(pipefd[1]);
 
-          // El nuevo hijo ejecuta el comando
-          if (execvp(args[0], args) == -1)
-          {
-            perror("Execvp failed");
-            exit(1);
-          }
-        }
-        else if (pid > 0)
-        {
-          // Es importante que las siguientes instrucciones no se ejecuten hasta que el hijo ejecutor termine su trabajo.
-          wait(NULL);
-        }
-      }
+      // Se cierra la entrada de escritura del padre.
+      close(pipefd[1]);
 
       // Se lee del pipe y se pasan estos bytes a la variable output.
       bytesRead = read(pipefd[0], output, sizeof(output) - 1);
 
-      // Si no se leyó nada del pipe (el comando no generó output), se envia el mensaje "Command Executed".
-      if (bytesRead > 0)
+      // Se cierra la entrada de lectura del padre.
+      close(pipefd[0]);
+
+      // Si no se leyó nada del pipe (el comando no generó output), se envia " ".
+      if (bytesRead == 0)
+      {
+        snprintf(output, sizeof(output), " ");
+      }
+      else
       {
         output[bytesRead] = '\0';
-      }
-      else if (bytesRead == -1 && strlen(output) == 0)
-      {
-        snprintf(output, sizeof(output), "Command executed\n");
       }
 
       // Se manda el output al cliente.
       send(clientSocket, output, strlen(output), 0);
     }
+  }
 
-    // Se cierra el descriptor de lectura del pipe en el proceso hijo original.
-    close(pipefd[0]);
-  }
-  else if (pid > 0) // Padre original.
-  {
-    // Se cierran los descriptures del pipe en el proceso padre original.
-    close(pipefd[0]);
-    close(pipefd[1]);
-    wait(NULL);
-    close(clientSocket);
-    close(servSockD);
-  }
+  close(clientSocket);
+  close(servSockD);
 
   return 0;
 }
